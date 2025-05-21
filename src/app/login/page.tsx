6275239fade249b2c3f29a24cc6a8a56b4bf5136
secret-coder489 <pragatitiwari678@gmail.com>
2025-05-22 01:20:54 +0530

    Changes done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -43,7 +43,7 @@ export default function Login() {
           required
         />
         <button type="submit" className="text-indigo-600 hover:text-white hover:bg-indigo-600 border border-indigo-600 px-4 py-2 rounded-md transition duration-200 w-full md:w-auto">
-           Log In
+          Log In
         </button>
       </form>
     </div>
